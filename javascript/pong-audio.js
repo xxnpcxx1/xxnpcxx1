e146fb889160bda7f561c8857e019f3402e03b0c
xxnpcxx1 <ethan.j.tittle@gmail.com>
2025-12-18 18:19:11 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -55,7 +55,7 @@ soundArray.push(scoreSound);
 export var ambientSound = new soundFile("generic-crowd-background-noise-31310.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -20; //turn down volume
+ambientSound.player.volume.value = -1; //turn down volume
 
 export var adventureMusic = new soundFile("=silence.mp3");
 soundArray.push(adventureMusic);
