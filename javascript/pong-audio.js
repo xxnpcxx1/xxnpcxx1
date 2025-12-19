661dd691724eed2f99df5fabe1448c7b754097bd
xxnpcxx1 <ethan.j.tittle@gmail.com>
2025-12-18 18:15:44 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -52,12 +52,12 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("silence.mp3");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("muffled-chatter-23025.mp3");
+export var ambientSound = new soundFile("generic-crowd-background-noise-31310.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -20; //turn down volume
 
-export var adventureMusic = new soundFile("muffled-chatter-23025.mp3");
+export var adventureMusic = new soundFile("=silence.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
 adventureMusic.player.volume.value = -16;
