d73564784ab9dcc69af4917bc200ffefde73140c
xxnpcxx1 <ethan.j.tittle@gmail.com>
2026-01-03 19:52:23 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -49,7 +49,7 @@ soundArray.push(wallSound); //add sound to list of sounds
 export var paddleSound = new soundFile("silence.mp3");
 soundArray.push(paddleSound);
 
-export var scoreSound = new soundFile("silence.mp3");
+export var scoreSound = new soundFile("old-clock-bell-27265.mp3");
 soundArray.push(scoreSound);
 
 export var ambientSound = new soundFile("generic-crowd-background-noise-31310.mp3");
